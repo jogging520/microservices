7607fc2defe7394b57874e6b835d4c17a05797ae
446734796@qq.com <wuqiang502>
2017-12-15 18:07:22 +0800

    写了部分的基础编排服务。

--- atom-resource-basic/src/main/java/com/northbrain/resource/basic/dto/IResourceDTO.java
+++ atom-resource-basic/src/main/java/com/northbrain/resource/basic/dto/IResourceDTO.java
@@ -2,6 +2,8 @@ package com.northbrain.resource.basic.dto;
 
 
 import com.northbrain.base.common.model.vo.ResourceVO;
+import com.northbrain.resource.basic.model.po.ResourceDetailHisPO;
+import com.northbrain.resource.basic.model.po.ResourceDetailPO;
 import com.northbrain.resource.basic.model.po.ResourceHisPO;
 import com.northbrain.resource.basic.model.po.ResourcePO;
 
